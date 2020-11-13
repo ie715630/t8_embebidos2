46017f5d21a767972af2049d89b2b67f270ae276
ie175630 <ie715630@iteso.mx>
2020-11-13 15:00:45 -0600

    Init implemented

--- WM8731.c
+++ WM8731.c
@@ -8,13 +8,28 @@
 #include "WM8731.h"
 
 #define DEVICE_ADDR 0x1A
+#define RESET_ADDR 0x0F
+#define LEFT_LINE_IN 0x00
+#define RIGHT_LINE_IN 0x01
+#define ANALOGUE_AUDIO 0x04
+#define LEFT_HP_OUT 0x02
+#define RIGHT_HP_OUT 0x03
 
 void init_codec()
 {
 
-    uint8_t reset_reg_as_data[] = {(0x0F<<1)};
-    i2c_multiple_write(DEVICE_ADDR, reset_reg_as_data, 1);
+    uint8_t i2c_data[2] = {RESET_ADDR, 0x00};
+    i2c_multiple_write(DEVICE_ADDR, i2c_data, 2);
+    
+    uint8_t db = 0; //0x00 (-34.5) - 0x1F (12)
+    db = (db+34.5)/1.5;
+    i2c_data[0] = LEFT_LINE_IN;
+    i2c_data[1] = db;
+    i2c_multiple_write(DEVICE_ADDR, i2c_data, 2);
 
-//    PRINTF("Reset success");
+    i2c_data[0] = RIGHT_LINE_IN;
+    i2c_data[1] = db;
+    i2c_multiple_write(DEVICE_ADDR, i2c_data, 2);
 
+    // uint8_t vol = 0x79 // 0dB: 0x30 (-73dB) - 0x7F (6dB)
 }
